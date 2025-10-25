a00edce204b912d2f1e1c1af36caa4f667418719
Rodrigo Nogueira <44399537+rodinogueira@users.noreply.github.com>
2025-10-25 10:56:30 -0300

    feat: change server port from 3000 to 8081

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  await app.listen(3000);
+  await app.listen(8081);
 }
 bootstrap();
